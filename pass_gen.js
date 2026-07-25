5c94c875e2dcb189954806923421546ac6a12433
infra-lead-45 <40@felix-au.me>
2026-07-25 15:49:00 +0530

    Implement features in pass_gen.js - Version 5

--- pass_gen.js
+++ pass_gen.js
@@ -5,3 +5,6 @@ function generatePassword(length = 12) {
     let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+    return password;
