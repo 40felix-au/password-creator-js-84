1447914e26d71c832d286c2df38f620dcc0e484e
infra-lead-45 <40@felix-au.me>
2026-07-25 15:48:59 +0530

    Implement features in pass_gen.js - Version 4

--- pass_gen.js
+++ pass_gen.js
@@ -1,3 +1,7 @@
 // Simple Password Generator in JavaScript
 
 function generatePassword(length = 12) {
+    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
